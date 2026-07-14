d3c7ba91ef04ae348c905e72b465c92152a73d7b
Ahmad Askari <71318738+askariahmad@users.noreply.github.com>
2026-07-14 15:35:15 +0530

    feat: add terraform for AKS deployment

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,87 @@
+﻿provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+}
+
+locals {
+  services = {
+    "dashboard-ui"         = "LoadBalancer"
+    "gateway-service"      = "LoadBalancer"
+    "config-service"       = "ClusterIP"
+    "incident-service"     = "ClusterIP"
+    "repo-scanner-service" = "ClusterIP"
+    "log-analyzer-service" = "ClusterIP"
+    "log-collector-service"= "ClusterIP"
+    "notification-service" = "ClusterIP"
+  }
+
+  env_vars = [
+    { name = "DB_HOST",       value = "http://localhost:4577" },
+    { name = "KAFKA_BROKERS", value = "http://localhost:4577" },
+    { name = "AUTH_URL",      value = "http://localhost:4577" }
+  ]
+}
+
+resource "kubernetes_deployment" "services" {
+  for_each = local.services
+
+  metadata {
+    name = each.key
+    labels = {
+      app = each.key
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = each.key
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = each.key
+        }
+      }
+      spec {
+        container {
+          name  = each.key
+          image = "devopsproacr.azurecr.io/{each.key}:latest"
+          
+          dynamic "env" {
+            for_each = local.env_vars
+            content {
+              name  = env.value.name
+              value = env.value.value
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "services" {
+  for_each = local.services
+
+  metadata {
+    name = each.key
+  }
+
+  spec {
+    selector = {
+      app = each.key
+    }
+    
+    type = each.value
+
+    port {
+      port        = 80
+      target_port = 8080
+    }
+  }
+}
---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+﻿terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  tenant_id       = "00000000-0000-0000-0000-000000000000"
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = "00000000-0000-0000-0000-000000000000"
+  metadata_host   = "http://localhost:4577"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-pro-rg"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "devops-pro-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "devopsproaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "devopsproacr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+output "kubeconfig" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
